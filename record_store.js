58c39a271770c9282c9063c9985293ed21e9a6ab
nickhillwd <nickhillwd@gmail.com>
2016-01-30 12:06:32 +0000

    add record to record store passing test green

--- record_store.js
+++ record_store.js
@@ -1,3 +1,13 @@
+var RecordStore = function(storeName, city){
+  this.storeName = storeName.toLowerCase();
+  this.city = city.toLowerCase();
+  this.inventory = [];
+}
 
+RecordStore.prototype = {
+  addRecord: function(record){
+    this.inventory.push(record);
+  }
+}
 
-// module.exports = RecordSztore;
+module.exports = RecordStore;
--- record_store_spec.js
+++ record_store_spec.js
@@ -4,7 +4,7 @@ var expect = chai.expect;
 var should = chai.should;
 
 var Record = require('./record.js');
-// var RecordStore = require('./record_store.js');
+var RecordStore = require('./record_store.js');
 
 describe('Record', function(){
   beforeEach(function(){
@@ -19,5 +19,33 @@ describe('Record', function(){
     expect(Record).itself.to.respondTo('title');
     Record.price = function(){};
     expect(Record).itself.to.respondTo('price');
-  })
+  });
 });
+
+describe('Record Store', function(){
+  beforeEach(function(){
+    store1 = new RecordStore("Rick's Revolutionary Record Store", "Edinburgh");
+    store2 = new RecordStore("Keith's Kool Tracks", "Edinburgh");
+    store3 = new RecordStore("Val's Vivaldi Emporium", "Edinburgh");
+    record1 = new Record("Spice Girls", "Spiceworld", 10.00);
+    record2 = new Record("Venga Boys", "The Psrty Album", "5.00");
+    record3 = new Record("Daphne and Clecste", "We Didn't Say That", 7.50);
+  });
+  afterEach(function(){
+    store1.inventory = [];
+    store2.inventory = [];
+    store3.inventory = [];
+  });
+  it('should accept 2 parameters; name, city', function(){
+    RecordStore.storeName = function(){};
+    expect(RecordStore).itself.to.respondTo('storeName');
+    RecordStore.city = function(){};
+    expect(RecordStore).itself.to.respondTo('city');
+  });
+  it('should accept new records into its inventory', function(){
+    store1.addRecord(record1);
+    store2.addRecord(record2);
+    expect(store1.inventory[0]).to.be.an('object');
+    expect(store2.inventory.length).to.eql(1);
+  });
+})
