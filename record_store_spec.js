a2290d96c728c05630b325ed8bba2b9ab62f27b0
nickhillwd <nickhillwd@gmail.com>
2016-01-30 10:29:03 +0000

    initial commit test enviroment setup

---
+++ record_store_spec.js
@@ -0,0 +1,8 @@
+var chai = require('./chai');
+var assert = chai.assert;
+var expect = chai.expect;
+var should = chai.should;
+
+var Record = require('./record.js');
+var RecordStore = require('./record_store.js');
+
